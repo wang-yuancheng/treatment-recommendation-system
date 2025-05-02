d67f9a6ca2f6568d9526b471be89515f83053478
wang-yuancheng <yuanchengntu@gmail.com>
2025-05-02 21:03:23 +0800

    Enable prediction pipeline to flask

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
-# code to load your trained model
+import os
+import joblib
+
+# Build the path to the saved pipeline
+BASE_DIR      = os.path.dirname(os.path.dirname(__file__)) # Get root of project (treatment-recommendation-system)
+PIPELINE_PATH = os.path.join(BASE_DIR, 'model', 'pipeline.pkl')
+
+# Load the pipeline at import time so it's ready for all routes
+pipeline = joblib.load(PIPELINE_PATH)
+
+# Expose only 'pipeline' for cleaner imports
+__all__ = ['pipeline']
--- app/routes/main/main.py
+++ app/routes/main/main.py
@@ -1,18 +1,38 @@
 from flask import Blueprint, render_template, request
-
+from app.models import pipeline
+import pandas as pd
 
 # Create the Blueprint object
 main_bp = Blueprint('main', __name__, template_folder='templates')
 
-new_patients = []
-
 # Define the routes on the blueprint
-@main_bp.route('/', methods=["GET", "POST"])
-def predict():
-    if request.method == "POST":
-        patient = {
-            'blood ': request.form['age'],
-            'gender': request.form['gender']
-        }
-        new_patients.append(patient)
+@main_bp.route('/', methods=["GET"])
+def home():
     return render_template('index.html')
+
+@main_bp.route('/predict', methods=["POST"])
+def predict():
+        data = request.form.get('csv_input')
+
+        #convert csv into expected input
+        age, bmi, sbp, dbp, chol_cat, gluc_cat = data.split(',')
+        age = float(age)
+        bmi = float(bmi)
+        sbp = float(sbp)
+        dbp = float(dbp)
+        chol_cat = int(chol_cat)
+        gluc_cat = int(gluc_cat)
+
+        df_user = pd.DataFrame([{
+            'age_years': age,
+            'body_mass_index': bmi,
+            'systolic_bp': sbp,
+            'diastolic_bp': dbp,
+            'cholesterol_level': chol_cat,
+            'glucose_level': gluc_cat
+        }])
+
+        proba = pipeline.predict_proba(df_user)[0, 1]  # returns class 1: probability of disease
+        print(f"Predicted cardiovascular risk: {proba:.3f}")
+
+        return render_template('predict.html', result=proba)
