a7e5913dd248c0e78e041e88b1c1c9080dd726ec
wang-yuancheng <yuanchengntu@gmail.com>
2025-05-05 21:49:00 +0800

    Improved user input form and add CSS

--- app/routes/main/main.py
+++ app/routes/main/main.py
@@ -12,27 +12,29 @@ def home():
 
 @main_bp.route('/predict', methods=["POST"])
 def predict():
-        data = request.form.get('csv_input')
+    data = {} #create a dictionary to store patient data
+    variables = ['age', 'gender', 'height', 'weight', 'sbp', 'dbp', 'chol', 'gluc', 'smoke', 'alcohol', 'active']
+    var_types = [float, int, float, float, float, float, int, int, int, int, int]
 
-        #convert csv into expected input
-        age, bmi, sbp, dbp, chol_cat, gluc_cat = data.split(',')
-        age = float(age)
-        bmi = float(bmi)
-        sbp = float(sbp)
-        dbp = float(dbp)
-        chol_cat = int(chol_cat)
-        gluc_cat = int(gluc_cat)
+    #transfer patient data to data dictionary
+    for var, var_type in zip(variables, var_types):
+        data[var] = request.form.get(var, type=var_type)
 
-        df_user = pd.DataFrame([{
-            'age_years': age,
-            'body_mass_index': bmi,
-            'systolic_bp': sbp,
-            'diastolic_bp': dbp,
-            'cholesterol_level': chol_cat,
-            'glucose_level': gluc_cat
-        }])
+    data['bmi'] = data['weight'] / (data['height'] ** 2) # BMI = Weight / Height^2
+    data['MAP'] = (data['sbp'] + 2 * data['dbp']) / 3    # Mean Arterial Pressure = (SBP + 2·DBP) / 3
+    data['PP'] = data['sbp'] - data['dbp']               # Pulse Pressure = SBP − DBP
 
-        proba = pipeline.predict_proba(df_user)[0, 1]  # returns class 1: probability of disease
-        print(f"Predicted cardiovascular risk: {proba:.3f}")
+    #for now, we only these values are expected by the model
+    df_user = pd.DataFrame([{
+        'age_years': data["age"],
+        'body_mass_index': data["bmi"],
+        'systolic_bp': data["sbp"],
+        'diastolic_bp': data["dbp"],
+        'cholesterol_level': data["chol"],
+        'glucose_level': data["gluc"]
+    }])
 
-        return render_template('predict.html', result=proba)
+    proba = pipeline.predict_proba(df_user)[0, 1]  # returns class 1: probability of disease
+    print(f"Predicted cardiovascular risk: {proba:.3f}")
+
+    return render_template('predict.html', result=proba)
